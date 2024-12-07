0905e82552cdc79c4bae488e3dfcdabfced78b35
shmint <148349626+swissmint@users.noreply.github.com>
2024-12-07 11:52:23 +0100

    Add files via upload

---
+++ nkl-software-installscript.sh
@@ -0,0 +1,36 @@
+flatpak install -y git
+flatpak install -y gparted
+flatpak install -y librewolf
+flatpak install -y gimp
+flatpak install -y zapzap
+sudo nala install tmux python3-pip cmatrix bpytop audacity -y
+flatpak install -y com.unity.UnityHub # Unity
+flatpak install -y com.vscodium.codium # Vscodium
+flatpak install -y org.freecadweb.FreeCAD # FreeCAD
+flatpak install -y xyz.xclicker.xclicker # Xclicker
+flatpak install -y com.logseq.Logseq # Logseq
+flatpak install -y io.freetubeapp.FreeTube # FreeTube
+flatpak install -y org.raspberrypi.rpi-imager # Raspberrypi-Imager
+flatpak install -y com.warlordsoftwares.youtube-downloader-4ktube # Youtube Downloader
+flatpak install -y com.anydesk.Anydesk # Anydesk
+flatpak install -y org.kde.kclock # Clockapp
+
+sudo nala update
+sudo nala upgrade
+
+
+# flatpak install -y virtualbox
+
+# flatpak install -y virt-manager
+# flatpak install -y qemu
+# flatpak install -y 
+# flatpak install -y 
+
+# flatpak install -y org.stande
+# flatpak install -y jp2a
+# flatpak install -y com.github.sdv43.whaler
+# flatpak install -y syncthing
+# flatpak install -y libreoffice-style-breeze
+# flatpak install -y tree
+# nano but modern : flatpak install -y micro 
+# photos making : flatpak install -y cheese
---
+++ nkl-software-updatescript.sh
@@ -0,0 +1,5 @@
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install flatpak nala
+sudo nala update -y
+sudo nala upgrade -y
